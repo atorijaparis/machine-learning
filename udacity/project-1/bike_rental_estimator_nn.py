58b4109068d35184322dcdf2cb571774cbfc1291
Alex Torija-Paris <alex.torija-paris@disneyanimation.com>
2017-04-14 21:37:45 -0700

    added matplotlib graph to standalone version + tweaks to notebook

--- udacity/project-1/bike_rental_estimator_nn.py
+++ udacity/project-1/bike_rental_estimator_nn.py
@@ -229,3 +229,81 @@ class TestMethods(unittest.TestCase):
 suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
 unittest.TextTestRunner().run(suite)
 
+import sys
+
+### Set the hyperparameters here ###
+iterations    = 800
+learning_rate = 0.1
+hidden_nodes  = 100
+output_nodes  = 1
+
+N_i = train_features.shape[1]
+network = NeuralNetwork(N_i, hidden_nodes, output_nodes, learning_rate)
+
+losses = {'train': [], 'validation': []}
+for ii in range(iterations):
+    # Go through a random batch of 128 records from the training data set
+    batch = np.random.choice(train_features.index, size=128)
+    X, y = train_features.ix[batch].values, train_targets.ix[batch]['cnt']
+
+    network.train(X, y)
+
+    # Printing out the training progress
+    train_loss = MSE(network.run(train_features).T, train_targets['cnt'].values)
+    val_loss = MSE(network.run(val_features).T, val_targets['cnt'].values)
+    sys.stdout.write("\rProgress: {:2.1f}".format(100 * ii / float(iterations)) \
+                     + "% ... Training loss: " + str(train_loss)[:5] \
+                     + " ... Validation loss: " + str(val_loss)[:5])
+    sys.stdout.flush()
+
+    losses['train'].append(train_loss)
+    losses['validation'].append(val_loss)
+
+#plt.plot(losses['train'], label='Training loss')
+#plt.plot(losses['validation'], label='Validation loss')
+#plt.legend()
+#_ = plt.ylim()
+
+fig, ax = plt.subplots()
+ax.plot(losses['train'], label='Training loss')
+ax.plot(losses['validation'], label='Validation loss')
+ax.grid(True)
+_ = plt.ylim()
+#ax.set_yscale('linear')
+#ax.set_autoscaley_on(True)
+
+# Now add the legend with some customizations.
+legend = ax.legend(shadow=True)
+
+# The frame is matplotlib.patches.Rectangle instance surrounding the legend.
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+
+# Set the fontsize
+for label in legend.get_texts():
+    label.set_fontsize('large')
+
+for label in legend.get_lines():
+    label.set_linewidth(1.5)  # the legend line width
+
+plt.show()
+
+
+fig, ax = plt.subplots(figsize=(8,4))
+mean, std = scaled_features['cnt']
+predictions = network.run(test_features).T*std + mean
+ax.plot(predictions[0], label='Prediction')
+ax.plot((test_targets['cnt']*std + mean).values, label='Data')
+ax.set_xlim(right=len(predictions))
+ax.legend()
+
+dates = pd.to_datetime(rides.ix[test_data.index]['dteday'])
+dates = dates.apply(lambda d: d.strftime('%b %d'))
+ax.set_xticks(np.arange(len(dates))[12::24])
+_ = ax.set_xticklabels(dates[12::24], rotation=45)
+plt.show()
+
+
+
+
+
